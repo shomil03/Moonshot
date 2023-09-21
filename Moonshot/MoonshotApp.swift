3cf8b396422b60a1959851cb28ad15a64f8aabda
shomil03 <shomil02020@gmail.com>
2023-09-21 21:53:09 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Shomil Singh on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
